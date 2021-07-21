3cd5df5fa4a16f394d3277fa37c319d459bb3d7d
NobleTheNoble <josenoel_n@yahoo.com.ph>
2021-07-21 21:42:27 +0800

    revisions to classes

---
+++ app/src/main/java/com/mobdeve/s12/group9/tetris/Game.java
@@ -0,0 +1,17 @@
+package com.mobdeve.s12.group9.tetris;
+
+import android.content.Context;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class Game {
+    protected Board brd;
+
+    public Game(Board b){
+        brd = b;
+    }
+
+    public void run(){
+
+    }
+}
--- app/src/main/java/com/mobdeve/s12/group9/tetris/MainActivity.java
+++ app/src/main/java/com/mobdeve/s12/group9/tetris/MainActivity.java
@@ -1,20 +1,32 @@
 package com.mobdeve.s12.group9.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-    @Override
+import java.net.CookieHandler;
 
+public class MainActivity extends AppCompatActivity {
+
+    protected Board b;
+    protected Game g;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Board board = new Board(this);
-        this.setContentView(board);
+        b = new Board(this);
+        g = new Game(b);
 
+        ConstraintLayout cl_board_layout = findViewById(R.id.cl_board_layout);
+        cl_board_layout.addView(b);
+
+        g.run();
+
+        //Board board = new Board(this);
+        //cl.addView(board);
         //board.drawBoard(new Canvas());
-
     }
 }
