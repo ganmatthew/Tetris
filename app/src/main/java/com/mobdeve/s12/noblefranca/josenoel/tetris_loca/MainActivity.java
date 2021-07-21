d0625d9b50b48a04f3ff5afa36d220f9cc95abe0
NobleTheNoble <josenoel_n@yahoo.com.ph>
2021-07-21 12:42:23 +0800

    testing (1)

---
+++ app/src/main/java/com/mobdeve/s12/noblefranca/josenoel/tetris_loca/MainActivity.java
@@ -0,0 +1,17 @@
+package com.mobdeve.s12.noblefranca.josenoel.tetris_loca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Board board = new Board(this);
+        this.setContentView(board);
+    }
+}
