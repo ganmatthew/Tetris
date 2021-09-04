f8ea73c4a49d5a53d8315830876c80db66171f91
ganmatthew <ganmatthew@users.noreply.github.com>
2021-09-04 22:53:37 +0800

    Minor additions to Board, Settings viewable, fullscreen mode

--- app/src/main/java/com/mobdeve/s12/group9/tetris/Board.java
+++ app/src/main/java/com/mobdeve/s12/group9/tetris/Board.java
@@ -2,8 +2,28 @@ package com.mobdeve.s12.group9.tetris;
 
 public class Board {
 
-    public Board () {
+    private int rows, columns;
+    private Block[][] grid;
 
+    public Board (int rows, int columns) {
+        this.rows = rows;
+        this.columns = columns;
+        createBoardGrid();
+    }
+
+    private void createBoardGrid() {
+        this.grid = new Block[this.rows][this.columns];
+
+        // Generate 2D array of blocks for the grid
+        for (int row = 0; row < this.rows; row++) {
+            for (int column = 0; column < this.columns; column++) {
+                this.grid[row][column] = new Block(row, column);
+            }
+        }
+    }
+
+    private Block getBlockCoords(Coordinates coords) {
+        return this.grid[coords.y][coords.x];
     }
 
 }
--- app/src/main/java/com/mobdeve/s12/group9/tetris/MainActivity.java
+++ app/src/main/java/com/mobdeve/s12/group9/tetris/MainActivity.java
@@ -1,6 +1,7 @@
 package com.mobdeve.s12.group9.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,7 +10,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private MediaPlayer mPlayer = new MediaPlayer();
 
     // Main menu view components
+    private ConstraintLayout clSettings;
     private Button btnPlay;
     private ImageButton btnSettings;
     private ImageButton btnScores;
@@ -53,14 +57,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Hide title bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        Objects.requireNonNull(getSupportActionBar()).hide();
-
-        // Make app fullscreen
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         // Initialize PreferenceManager
         this.sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
@@ -70,11 +66,14 @@ public class MainActivity extends AppCompatActivity {
         this.btnSettings = findViewById(R.id.btn_menu_settings);
         this.btnScores = findViewById(R.id.btn_menu_leaderboard);
 
+        this.clSettings = findViewById(R.id.cl_menu_settings_container);
+
         // Plays the Tetris theme
         musicStart();
 
         // Listen to menu buttons
         bindToPlayButton();
+        bindToSettingsButton();
     }
 
     // Binds the play button to launch the board activity
@@ -87,9 +86,38 @@ public class MainActivity extends AppCompatActivity {
 
     // Binds the settings button to launch the settings view
     private void bindToSettingsButton() {
-        btnSettings.setOnClickListener(v -> {
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_settings, null);
 
+        btnSettings.setOnClickListener(v -> {
+            if (view.getParent() == null)
+                clSettings.addView(view);
         });
+
+        Button closeButton = view.findViewById(R.id.btn_settings_exit);
+        view.setLayoutParams(new ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT)
+        );
+
+        closeButton.setOnClickListener(v -> {
+            if (view.getParent() != null)
+                clSettings.removeView(view);
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
     }
 
     /***
