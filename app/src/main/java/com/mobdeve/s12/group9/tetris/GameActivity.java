5b753ec40309666ea7f2efca83a0b72b3eca8419
NobleTheNoble <josenoel_n@yahoo.com.ph>
2021-09-13 16:17:28 +0800

    changed ClearLines to int function

--- app/src/main/java/com/mobdeve/s12/group9/tetris/GameActivity.java
+++ app/src/main/java/com/mobdeve/s12/group9/tetris/GameActivity.java
@@ -541,11 +541,13 @@ public class GameActivity extends AppCompatActivity implements GestureDetector.O
         return true;
     }
 
-    public void ClearLines(){
+    public int ClearLines(){
+        int rt_val = 0;
+
         for (int i = NUM_HEIGHT - 1; i > -1; i--){  //from the bottom of the grid, go up
 
             if (ClearLines_Sub(blockData[i])){ //until you find a row that is full
-
+                rt_val++;
                 for (int j = i; j > 0; j--){ // if so, loop until the highest point
 
                     for (int k = 0; k < NUM_WIDTH; k++){
@@ -556,6 +558,8 @@ public class GameActivity extends AppCompatActivity implements GestureDetector.O
             }
 
         }
+
+        return rt_val;
     }
 
     /***
